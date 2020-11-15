61a38e5ff846348ea603da4485951063dc9c0a6d
SavaLione <monologuesplus@gmail.com>
2020-11-15 12:52:15 +0500

    LICENSE

--- src/platform/platform.cpp
+++ src/platform/platform.cpp
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: BSD-3-Clause
  * 
- * Copyright (c) 2020 Savely Pototsky (SavaLione)
+ * Copyright (c) 2020, Savely Pototsky (SavaLione)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
